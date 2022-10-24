13b8f484318366e6f8102fa28c2df10f6ab193f9
slothsoft <admin@slothsoft.de>
2022-10-24 22:23:17 +0200

    [#15] trying (and failing) to calculate fruit trees correctly

---
+++ Informant/Implementation/Common/Constants.cs
@@ -0,0 +1,17 @@
+﻿// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable UnusedMember.Global
+// ReSharper disable UnusedType.Global
+namespace Slothsoft.Informant.Implementation.Common;
+
+/// <summary>
+/// Contains constants for all the seasons.
+/// </summary>
+internal static class Seasons {
+    public const string Spring = "spring";
+    public const string Summer = "summer";
+    public const string Fall = "fall";
+    public const string Winter = "Winter";
+    public static readonly string[] All = { Spring, Summer, Fall, Winter };
+    
+    public const int LengthInDays = 28;
+}
