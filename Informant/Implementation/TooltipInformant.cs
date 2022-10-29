19c6460d61c075db2d222129b0903f716f687a63
slothsoft <admin@slothsoft.de>
2022-10-29 20:41:14 +0200

    [#9] allow multiple tooltip generators

--- Informant/Implementation/TooltipInformant.cs
+++ Informant/Implementation/TooltipInformant.cs
@@ -17,7 +17,7 @@ internal class TooltipInformant : ITooltipInformant<TerrainFeature>, ITooltipInf
     private BaseTooltipInformant<TerrainFeature>? _terrainFeatureInformant;
     private BaseTooltipInformant<SObject>? _objectInformant;
     
-    private Tooltip? _tooltip;
+    private IEnumerable<Tooltip>? _tooltips;
 
     public TooltipInformant(IModHelper modHelper) {
         _modHelper = modHelper;
@@ -36,14 +36,13 @@ internal class TooltipInformant : ITooltipInformant<TerrainFeature>, ITooltipInf
         if (!Context.IsPlayerFree) {
             return;
         }
-        _tooltip = GenerateObjectTooltip();
-        _tooltip ??= GenerateTerrainFeatureTooltip();
+        _tooltips = GenerateObjectTooltips().Concat(GenerateTerrainFeatureTooltips());
     }
 
-    private Tooltip? GenerateTerrainFeatureTooltip() {
+    private IEnumerable<Tooltip> GenerateTerrainFeatureTooltips() {
         if (_terrainFeatureInformant == null) {
             // if there is no generator in that, we don't need to do anything further
-            return null;
+            return Enumerable.Empty<Tooltip>();
         }
         
         var mousePosX = (Game1.getOldMouseX() + Game1.viewport.X) / Game1.tileSize;
@@ -53,13 +52,13 @@ internal class TooltipInformant : ITooltipInformant<TerrainFeature>, ITooltipInf
             Game1.currentLocation.terrainFeatures.Values
                 .Where(t => mousePosX == (int) t.currentTileLocation.X && mousePosY == (int) t.currentTileLocation.Y)
                 .ToArray()
-        ).SingleOrDefault();
+        );
     }
     
-    private Tooltip? GenerateObjectTooltip() {
+    private IEnumerable<Tooltip> GenerateObjectTooltips() {
         if (_objectInformant == null) {
             // if there is no generator in that, we don't need to do anything further
-            return null;
+            return Enumerable.Empty<Tooltip>();
         }
         
         var mousePosX = (Game1.getOldMouseX() + Game1.viewport.X) / Game1.tileSize;
@@ -69,21 +68,33 @@ internal class TooltipInformant : ITooltipInformant<TerrainFeature>, ITooltipInf
             Game1.currentLocation.netObjects.Values
                 .Where(t => mousePosX == (int) t.TileLocation.X && mousePosY == (int) t.TileLocation.Y)
                 .ToArray()
-        ).SingleOrDefault();
+        );
     }
 
     private void OnRendered(object? sender, RenderedEventArgs e) {
-        if (Context.IsPlayerFree && _tooltip != null) {
-            DrawSimpleTooltip(Game1.spriteBatch, _tooltip.Text, Game1.smallFont);
+        if (Context.IsPlayerFree && _tooltips != null) {
+            var tooltipsArray = _tooltips.ToArray();
+
+            if (tooltipsArray.Length == 0) {
+                return;
+            }
+            const int borderSize = 3 * Game1.pixelZoom;
+            int? startY = null;
+            var font = Game1.smallFont;
+            var tooltipsWidth = (int) tooltipsArray.Select(t => font.MeasureString(t.Text).X).Max() + Game1.tileSize / 2;
+            
+            foreach (var tooltip in tooltipsArray) {
+                var bounds = DrawSimpleTooltip(Game1.spriteBatch, tooltip, font, startY, tooltipsWidth);
+                startY = bounds.Y + bounds.Height - borderSize;
+            }
         }
     }
 
-    private static void DrawSimpleTooltip(SpriteBatch b, string hoverText, SpriteFont font) {
-        var textSize = font.MeasureString(hoverText);
-        var width = (int) textSize.X + Game1.tileSize / 2;
+    private static Rectangle DrawSimpleTooltip(SpriteBatch b, Tooltip tooltip, SpriteFont font, int? startY, int width) {
+        var textSize = font.MeasureString(tooltip.Text);
         var height = Math.Max(60, (int) textSize.Y + Game1.tileSize / 2);
         var x = Game1.getOldMouseX() + Game1.tileSize / 2;
-        var y = Game1.getOldMouseY() + Game1.tileSize / 2;
+        var y = startY ?? Game1.getOldMouseY() + Game1.tileSize / 2;
 
         if (x + width > Game1.viewport.Width) {
             x = Game1.viewport.Width - width;
@@ -95,13 +106,17 @@ internal class TooltipInformant : ITooltipInformant<TerrainFeature>, ITooltipInf
             y = Game1.viewport.Height - height;
         }
 
-        IClickableMenu.drawTextureBox(b, Game1.menuTexture, new Rectangle(0, 256, 60, 60), x, y, width, height, Color.White);
+        var textureBoxBounds = new Rectangle(x, y, width, height);
+        IClickableMenu.drawTextureBox(b, Game1.menuTexture, new Rectangle(0, 256, 60, 60), textureBoxBounds.X, textureBoxBounds.Y, 
+            textureBoxBounds.Width, textureBoxBounds.Height, Color.White);
 
         var position = new Vector2(x + Game1.tileSize / 4, y + Game1.tileSize / 4 + 4);
-        b.DrawString(font, hoverText, position + new Vector2(2f, 2f), Game1.textShadowColor, 0, Vector2.Zero, 1f, SpriteEffects.None, 0);
-        b.DrawString(font, hoverText, position + new Vector2(0f, 2f), Game1.textShadowColor, 0, Vector2.Zero, 1f, SpriteEffects.None, 0);
-        b.DrawString(font, hoverText, position + new Vector2(2f, 0f), Game1.textShadowColor, 0, Vector2.Zero, 1f, SpriteEffects.None, 0);
-        b.DrawString(font, hoverText, position, Game1.textColor * 0.9f, 0, Vector2.Zero, 1f, SpriteEffects.None, 0);
+        b.DrawString(font, tooltip.Text, position + new Vector2(2f, 2f), Game1.textShadowColor, 0, Vector2.Zero, 1f, SpriteEffects.None, 0);
+        b.DrawString(font, tooltip.Text, position + new Vector2(0f, 2f), Game1.textShadowColor, 0, Vector2.Zero, 1f, SpriteEffects.None, 0);
+        b.DrawString(font, tooltip.Text, position + new Vector2(2f, 0f), Game1.textShadowColor, 0, Vector2.Zero, 1f, SpriteEffects.None, 0);
+        b.DrawString(font, tooltip.Text, position, Game1.textColor * 0.9f, 0, Vector2.Zero, 1f, SpriteEffects.None, 0);
+
+        return textureBoxBounds;
     }
     
     public void Add(ITooltipGenerator<TerrainFeature> generator) {
