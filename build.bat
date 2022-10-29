9feca987713e43b1f96153e98be2ada368ef6efc
slothsoft <admin@slothsoft.de>
2022-10-29 22:55:30 +0200

    [#13] copy nexus mod release scripts

--- build.bat
+++ build.bat
@@ -3,32 +3,18 @@
 Rem Release the entire solution
 dotnet publish -c Release
 
-Rem Clear target folder
-rmdir "%cd%\bin" /s /q
-mkdir "%cd%\bin"
-
-Rem Build all the projects
-CALL :CreateZip Informant %~1
-
-%SystemRoot%\explorer.exe "%cd%\bin"
-EXIT /B %ERRORLEVEL%
-
-
-:CreateZip
-set projectName=%~1
-
 Rem Create a folder that contains everything that should be in the ZIP
-set projectFolder=%cd%\%projectName%
+set projectFolder=%cd%\Informant
 set zipFolder=%projectFolder%\bin\ReleaseZip
-set outputFolder=%zipFolder%\%projectName%
+set outputFolder=%zipFolder%\Informant
 if exist "%zipFolder%" rmdir /s /q "%zipFolder%"
 if not exist "%outputFolder%" mkdir "%outputFolder%"
 
-xcopy /y %projectFolder%\bin\Release\net5.0\publish\%projectName%.dll %outputFolder%
-xcopy /y %projectFolder%\bin\Release\net5.0\publish\%projectName%.pdb %outputFolder%
+xcopy /y %projectFolder%\bin\Release\net5.0\publish\Informant.dll %outputFolder%
+xcopy /y %projectFolder%\bin\Release\net5.0\publish\Informant.pdb %outputFolder%
 xcopy /y %projectFolder%\manifest.json %outputFolder%
-xcopy /y %projectFolder%\assets\ %outputFolder%\assets\
 xcopy /y %projectFolder%\i18n\ %outputFolder%\i18n\
+xcopy /y %projectFolder%\assets\ %outputFolder%\assets\
 xcopy /y %cd%\LICENSE %outputFolder%
 
 Rem Make a HTML file out of the README
@@ -36,8 +22,24 @@ Rem If this stops working, maybe run "choco install pandoc" on the PowerShell ag
 pandoc %cd%\README.md -t html -o %outputFolder%\Readme.html -s --metadata title="Slothsoft Informant"
 
 Rem Replace the image URLs of the HTML
-powershell -Command "((gc %outputFolder%\Readme.html -encoding utf8) -replace 'readme/dev', 'https://github.com/slothsoft/stardew-informant/blob/main/readme/dev' -replace 'readme/screen', 'https://github.com/slothsoft/stardew-informant/raw/main/readme/screen') | Out-File -encoding utf8 %outputFolder%\Readme.html"
+powershell -Command "((gc %outputFolder%\Readme.html -encoding utf8) -replace 'readme/dev', 'https://github.com/slothsoft/stardew-Informant/blob/main/readme/dev' -replace 'readme/screen', 'https://github.com/slothsoft/stardew-Informant/raw/main/readme/screen') | Out-File -encoding utf8 %outputFolder%\Readme.html"
+
+Rem Clear target folder
+rmdir "%cd%\bin" /s /q
+mkdir "%cd%\bin"
 
 Rem Now zip the entire folder
-"C:\Program Files\7-Zip\7z.exe" a %cd%\bin\%projectName%-%2.zip %zipFolder%/*
-EXIT /B 0
+"C:\Program Files\7-Zip\7z.exe" a %cd%\bin\Informant-%1.zip %zipFolder%/*
+
+
+
+Rem Create Readme Files for Nexus
+call build/create-plaintext.bat %cd%\README.md %cd%\bin\Readme.txt
+call build/create-bbcode.bat %cd%\README.md %cd%\bin\bbcode.txt
+xcopy /y %cd%\bin\bbcode.txt %cd%\readme\
+xcopy /y %cd%\readme\bbcode-version.txt %cd%\bin\
+
+
+
+Rem Open the folder with the results
+%SystemRoot%\explorer.exe "%cd%\bin"
