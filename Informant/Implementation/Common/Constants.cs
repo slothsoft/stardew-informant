bcd16db76d939249fe5fbcb3c0bcc19659609289
slothsoft <admin@slothsoft.de>
2022-11-05 08:49:48 +0100

    [#38] fix spelling mistake

--- Informant/Implementation/Common/Constants.cs
+++ Informant/Implementation/Common/Constants.cs
@@ -10,7 +10,7 @@ internal static class Seasons {
     public const string Spring = "spring";
     public const string Summer = "summer";
     public const string Fall = "fall";
-    public const string Winter = "Winter";
+    public const string Winter = "winter";
     public static readonly string[] All = { Spring, Summer, Fall, Winter };
     
     public const int LengthInDays = 28;
